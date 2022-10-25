2aead7b69a6e9cfc3109e9912fc89d7e9395369c
Tchakah Koffi <koffitchak@gmail.com>
2022-10-25 09:52:17 +0200

    les entités

---
+++ src/main/java/fr/vde/persist/entities/Etudiant.java
@@ -0,0 +1,77 @@
+package fr.vde.persist.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+
+
+@Table(name="STUDENT") 
+@Entity
+public class Etudiant {
+
+public Etudiant()  {}
+
+@Id
+@Column(name="ID")  
+private long id ;
+@Column(name="NAME") 
+private String nom;  
+@Column(name="FIRSTNAME")   
+private String prenom; 
+@Column(name="AGE")  
+private int age;
+@Column(name="DATEINSCRIPTION") 
+private String dateInscription; 
+
+
+
+
+public long getId()   
+{    
+return id;    
+}    
+public void setId(long id)   
+{    
+this.id = id;
+
+
+public String getNom()   
+{    
+return nom;    
+}    
+public void setNom(String nom)   
+{    
+this.nom = nom; 
+}
+
+public String getPrenom()   
+{    
+return prenom;    
+}  
+public void setPrenom(String prenom)   
+{    
+this.prenom = prenom; 
+}
+public int getAge()   
+{    
+return age;    
+}    
+public void setAge(long age)   
+{    
+this.age = age;
+
+
+public String getDateInscription()   
+{    
+return dateInscription;    
+}    
+public void setDateInscription(String dateInscription)   
+{    
+this.dateInscription= dateInscription; 
+
+
+
+
+}
